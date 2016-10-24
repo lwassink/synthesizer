b6296f1c86baa46b51d119e90ac281ea2c467b6c
App Academy Student <student@appacademy.io>
2016-10-24 12:16:01 -0700

    lunch commit

---
+++ frontend/components/synth/synth_container.jsx
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import { keyReleased, keyPressed } from '../../actions/note_actions';
+import Synth from './synth';
+
+const mapStateToProps = (state) => ({
+  notes: state.notes
+});
+
+const mapDispatchToProps = dispatch => ({
+  keyPressed: key => dispatch(keyPressed(key)),
+  keyReleased: key => dispatch(keyReleased(key))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Synth);
--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Note from './util/note';
+// import Note from './util/note';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  
-  ReactDOM.render(<p>I'm react</p>,
-  document.getElementById('root'));
+
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(< Root store={store} />, rootEl);
 });
